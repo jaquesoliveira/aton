dfd6240d9fdcbe7b62c4c7bbd5ea2e1b1892e1b3
Jaques Oliveira <jaques.oliveira@gmail.com>
2024-06-08 08:55:01 -0300

    configurações de variáveis de ambiente

--- src/app/enviroment/enviroment.energy.ts
+++ src/app/enviroment/enviroment.energy.ts
@@ -1,8 +1,8 @@
 export const enviroment = {
     production: false,
     //baseUrl: "http://192.168.100.55:8080"
-    baseUrl: "http://192.168.100.29:8080" //laptop casa
+    //baseUrl: "http://192.168.100.29:8080" //laptop casa
     //baseUrl: "http://192.168.1.84:8080"
     //baseUrl: "http://localhost:8080"
-    //baseUrl: "http://192.168.15.60:8080"
+    baseUrl: "http://192.168.15.60:8080"
 }
