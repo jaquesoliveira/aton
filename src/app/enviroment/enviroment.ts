38617ccb887e0d3239b1e780b589878ea49b877e
jaques <jaques.oliveira@gmail.com>
2024-06-07 21:37:12 -0300

    reformulação da tela de cadastro de cliente

--- src/app/enviroment/enviroment.ts
+++ src/app/enviroment/enviroment.ts
@@ -1,6 +1,7 @@
 export const enviroment = {
     production: false,
-    baseUrl: "http://192.168.100.55:8080"
+    baseUrl: "http://192.168.100.55:8080",
+    urlApiCep: "https://viacep.com.br/ws/"
     //baseUrl: "http://192.168.100.26:8080"
     //baseUrl: "http://192.168.1.84:8080"
     //baseUrl: "http://localhost:8080"
---
+++ src/app/services/cep.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { enviroment } from '../enviroment/enviroment';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CepService {
+
+  private url = enviroment.urlApiCep;
+
+  //https://viacep.com.br/ws/61934260/json/
+
+  httpOptions =  {
+    headers: new HttpHeaders({'Content-Type': 'application/json;charset=UTF-8'})
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  consultar(cep: string){
+    return this.httpClient.get<any>(this.url.concat(`${cep}/json`))
+    .pipe(catchError(this.handlerError))
+  }
+
+  handlerError(error: HttpErrorResponse){
+    let errorMessage = ''
+
+    if(error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message
+    } else {
+      errorMessage = "Deu ruim"
+    }
+    return throwError(errorMessage);
+  }
+}
